e00a0c5d8ea60dda46c48dc13c8bfd71220daec7
Rafael Ghinato <rga@cesar.school>
2022-10-09 00:03:08 -0300

    Primeiras configs

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': 'off',
+  },
+};
--- App.js
+++ App.js
@@ -1,21 +1,28 @@
+/* eslint-disable react/style-prop-object */
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: 'orange',
+      alignItems: 'center',
+      justifyContent: 'center',
+      color: 'orange',
+    },
+  });
+
+  const styleText = StyleSheet.create({
+    color: 'white',
+    fontSize: 40,
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styleText}>Te amo gorda!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
